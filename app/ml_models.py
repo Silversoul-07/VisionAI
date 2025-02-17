d84af4fc36995da39deb25a2e9d8590fcf00c849
Praveen Kumar <praveenkumar.e2022@vitstudent.ac.in>
2025-02-17 13:34:29 +0530

    GPU support

--- app/ml_models.py
+++ app/ml_models.py
@@ -9,15 +9,16 @@ def init_models():
     
     # find lighter models for faster inference and higher accuracy
     detector = YOLO('models/yolov8l.pt')
+    detector.to('cuda')
     
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    print(f"Using {device} device")
+    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # print(f"Using {device} device")
 
     # experiment eith different algorithms, weights, and configurations
     tracker = BotSort(
-        reid_weights=Path('models/osnet_ibn_x1_0_msmt17.pt'),
-        device=device,
-        half=False, # setup gpu and use half precision
+        reid_weights=Path('models/osnet_x0_50_msmt17.pt'),
+        device='cuda',
+        half=True, # setup gpu and use half precision
     )
     
     return detector, tracker
